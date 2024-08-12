6fb3e27dea218e5459d6274a36ff03901ba773c0
jalajmalhotra16 <121757198+jalajmalhotra16@users.noreply.github.com>
2024-08-12 09:08:13 +0530

    Update output.tf

--- assignment-2/output.tf
+++ assignment-2/output.tf
@@ -10,6 +10,11 @@ output "private_subnet_ids" {
   value = module.vpc.private_subnet_ids
 }
 
+output "private_subnet_for_db_ids" {
+  value = module.vpc.private_subnet_ids_for_db
+}
+
+
 output "nat_gateway_id" {
   value = module.vpc.nat_gateway_id
 }
